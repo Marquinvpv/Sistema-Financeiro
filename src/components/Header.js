4eb66c5c97f1d44fb0164c43ba58c932dd221c90
Marcus Vinicius <marcus.vpv2@gmail.com>
2023-07-15 15:57:14 -0300

    Feat: Create Header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "../img/logo.png";
+import "../css/Header.css";
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <div className="header-children">
+        <img className="logo" src={Logo} alt="logo" />
+        <h1>Sistema Financeiro</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
